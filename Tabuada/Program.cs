2a01157eb43538d415f55d682eb16b2723e45b6b
CalebeSilveira <calebe_mendes.silveira@hotmail.com>
2021-08-19 17:35:25 -0300

    Tabuada - Avaliação Aval Group

---
+++ Tabuada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int e = 1; e <= 10; e++)
+            {
+                for (int d = 0; d <= 10; d++)
+                {
+                    Console.WriteLine(e + " X " + d + " = " + e * d);
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
